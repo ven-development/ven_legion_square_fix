8919afc3d4d6a7f38a0dfd3365e9a1f8f9f9a120
VenDevelopment <thevendettaop@gmail.com>
2024-12-12 19:36:39 +0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/dt1_18_0.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dt1_18_build3.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/hei_dt1_23_0.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/hi@dt1_18_0.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/hi@hei_dt1_23_0.ytyp'
